dfb4944181e58a5d48653f81d9edb0b511f1ab51
Diogo Fabricio <diogo.felizardo@hotmail.com>
2017-01-08 03:53:30 -0300

    Exemplo finalizado
    plugins adicionados e exemplo finalizado.

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'firebase'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,4 +21,55 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+
+.factory('Auth', function($firebaseAuth){
+    // Initialize Firebase colocar suas configurações aqui
+    var config = {
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      storageBucket: "",
+      messagingSenderId: ""
+    };
+    return firebase.initializeApp(config);
+})
+
+
+.controller('AppCtrl', function($scope, Auth,$firebaseAuth){
+  
+ 
+  $scope.LoginFacebook = function(){
+    var provider = new firebase.auth.FacebookAuthProvider();
+    
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // Usuario logado      
+    }).catch(function(error) {
+        // An error occurred
+        console.log(error);
+    });
+  };  
+
+  $scope.getUser = function(){       
+
+    var user = firebase.auth().currentUser;
+    if (user) {
+        // Usuario logado
+        $scope.usuario = user;                    
+    } else {
+        // Usuario deslogado
+        $scope.usuario = "";
+    }
+  };  
+
+
+  $scope.Logout = function(){   
+    firebase.auth().signOut().then(function() {
+      console.log("Usuario deslogado");
+      $scope.usuario = "";
+    }).catch(function (error){
+      console.log(error);
+    });    
+  };  
+
 })
